40e061727da12a863fa7e623b7dca6839d9de200
mateogallardoo <mateo.gallardo@inacapmail.cl>
2022-09-29 11:07:57 -0300

    trabajo de uber

---
+++ poo.py
@@ -0,0 +1,11 @@
+print ("escribe tu nombre")
+edad = int(input("imgresa tu edad"))
+print("el año prox tendras"+ str(edad+1)+ "años")
+if(edad<=18):
+    print("es menor de edad")
+elif
+    print("es mayor de edad")
+i = 0
+while (i<20):
+    print("es un ciclo "+ str(i))
+    i = i + 1 
